5f9c5b56bdbbd488dabdbfc9e658a0c8dc27455e
gustavogds <gustavogds142000@gmail.com>
2023-07-12 15:10:19 -0300

    Add students management

--- students/models.py
+++ students/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    student_id = models.PositiveIntegerField()
+    student_number = models.PositiveIntegerField()
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
--- students/urls.py
+++ students/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    
+    path('<int:id>', views.viewStudent, name='viewStudent'),
+    path('add/', views.add, name='add'),
+    path('edit/<int:id>', views.edit, name='edit'),
+    path('delete/<int:id>', views.delete, name='delete')
 ]
